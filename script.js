a065f173c447012896c5d15d3ef9710b08b5f5b3
karan <ks888570@gmail.com>
2023-11-09 20:13:44 +0530

    designed home page and footer

---
+++ script.js
@@ -0,0 +1,16 @@
+var githubButton = document.getElementById('github');
+    var linkedinButton = document.getElementById('linkedin');
+    var twitterButton = document.getElementById('twitter');
+
+    // Add click event listeners
+    githubButton.addEventListener('click', function() {
+      window.location.href = 'https://github.com/Karansi13';
+    });
+
+    linkedinButton.addEventListener('click', function() {
+      window.location.href = 'https://www.linkedin.com/in/karan-singh-kalsi-589006255/';
+    });
+
+    twitterButton.addEventListener('click', function() {
+      window.location.href = 'https://twitter.com/KaranSi03511382';
+    });
